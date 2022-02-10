8bf79e438a8e6bb68eaa92aee0ff6aad7eb47cef
Pierre <p.drezet@inx-systems.com>
2022-02-10 13:41:34 +0000

    Try this sucker!

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 int main(int argc, char** argv) {
-  printf("Hello World from %s\n",argv[0]);
-  #error "or not"
+  printf("Hello World from %s\n",argv[5]); 
+  #warning "or not"
 }
